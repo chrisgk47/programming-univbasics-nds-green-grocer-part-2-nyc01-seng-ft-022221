f9660498a446d2cbc0831634eeea2b5afdea47e1
Christopher Kim <chris.kim1990@gmail.com>
2021-02-19 23:27:24 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,18 +1,44 @@
 require_relative './part_1_solution.rb'
+require 'pry'
+#cart = [{:item=>"AVOCADO", :price=>3.0, :clearance=>true, :count=>2}]
+#coupons = [{:item=>"AVOCADO", :num=>2, :cost=>5.0}]
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  new_cart = []
+  coupons.each do |coupon|
+    name = coupon[:item]
+    cart.each do |incart|
+      if incart[:item] == name && incart[:count] >= coupon[:num]
+        new_name = "#{name} W/COUPON"
+        new_hash = {
+          :item => new_name,
+          :price => coupon[:cost]/coupon[:num],
+          :clearance => incart[:clearance],
+          :count => coupon[:num]
+        }
+        incart[:count] -= new_hash[:count]
+        cart << new_hash
+      end
+    end
+  end
+  cart
 end
+
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  #binding.pry
+  new_cart =[]
+  cart.each do |item|
+    #binding.pry
+    if item[:clearance] == true
+      item[:price] = (item[:price] * 0.8).round(2)
+    end
+  end
 end
 
+
 def checkout(cart, coupons)
+  binding.pry
   # Consult README for inputs and outputs
   #
   # This method should call
